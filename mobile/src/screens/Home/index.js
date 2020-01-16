adf3bba376dc4732314128debe584181906dd023
RennanD <rplaykl@gmail.com>
2020-01-16 17:52:53 -0200

    Init app mobile

---
+++ mobile/src/screens/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import {Container} from './styles';
+
+export default function Home() {
+  return <Container />;
+}
+
+Home.navigationOptions = {
+  title: 'Find Devs',
+};
