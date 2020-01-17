5aacad93a05496284b681b5f8918983b665484ec
RennanD <rplaykl@gmail.com>
2020-01-17 19:22:41 -0300

    Remove mobile

--- mobile/App.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import Routes from "./src/routes";
-
-export default function App() {
-  return <Routes />;
-}
--- mobile/src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://192.168.15.191:3001"
-});
-
-export default api;
